0f64152f75f740b6c995976aa56eda2df531e2a7
shelbykohring <71856110+kohringsw@users.noreply.github.com>
2020-12-17 22:17:15 -0500

    update with HTML add routes and fetching data

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// path to allow access to public directories
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -116,6 +118,18 @@ app.post("/api/animals", (req, res) => {
     res.json(animal);
   }
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+})
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+})
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
